1775db9abb0c6088224270ad8543a199b31fd0a2
hgerykova <256982@vutbr.cz>
2024-04-24 09:30:30 +0200

    fibofunkce

--- fibo.py
+++ fibo.py
@@ -1,6 +1,19 @@
+def recursive_nth_fibo(number):
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(number - 1) + recursive_nth_fibo(number - 2)
+
 
 def main():
-    pass
+    number = int(input("vlozte cisla:"))
+    seq = []
+    for num in range(number):
+        seq.append(recursive_nth_fibo(num))
+    print(seq)
+
 
 
 if __name__ == "__main__":
